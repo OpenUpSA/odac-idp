57d8ba55c1facd1749927421fca401105d006d70
Petrus J.v.Rensburg <petrus.jvrensburg@gmail.com>
2014-03-18 15:29:49 +0200

    Convert config and fabfile to project-template format.

--- fabfile.py
+++ fabfile.py
@@ -1,20 +1,14 @@
 from __future__ import with_statement
 from fabric.api import *
+from fabdefs import *
 
 
-def staging():
+def set_permissions():
     """
-    Env parameters for the staging environment.
+    Ensure user www-data has access to the application folder.
     """
-
-    # env.hosts = ['ec2-54-194-218-89.eu-west-1.compute.amazonaws.com']
-    env.hosts = ['54.229.255.34']
-    env.envname = 'staging'
-    env.user = 'ubuntu'
-    env.group = 'ubuntu'
-    env.key_filename = '~/.ssh/aws_code4sa.pem'
-    env['config_dir'] = 'config_staging'
-    print("STAGING ENVIRONMENT\n")
+    sudo('chown -R www-data:www-data ' + env.project_dir)
+    sudo('chmod -R 775 ' + env.project_dir)
     return
 
 
@@ -59,47 +53,6 @@ def setup():
     return
 
 
-def deploy():
-    """
-    Upload our package to the server.
-    """
-
-    # create application directory if it doesn't exist yet
-    with settings(warn_only=True):
-        if run("test -d /var/www/odac-idp").failed:
-            # create project folder
-            sudo('mkdir -p /var/www/odac-idp')
-
-    # create a tarball of our package
-    local('tar -czf frontend.tar.gz frontend/', capture=False)
-
-    # upload the source tarball to the temporary folder on the server
-    put('frontend.tar.gz', '/tmp/frontend.tar.gz')
-
-    # enter application directory
-    with cd('/var/www/odac-idp'):
-        # and unzip new files
-        sudo('tar xzf /tmp/frontend.tar.gz')
-
-    # now that all is set up, delete the tarball again
-    sudo('rm /tmp/frontend.tar.gz')
-    local('rm frontend.tar.gz')
-
-    sudo('touch /var/www/odac-idp/frontend/uwsgi.sock')
-
-    # clean out old logfiles
-    with settings(warn_only=True):
-        sudo('rm /var/www/odac-idp/frontend/debug.log*')
-
-    # ensure user www-data has access to the application folder
-    sudo('chown -R www-data:www-data /var/www/odac-idp')
-    sudo('chmod -R 775 /var/www/odac-idp')
-
-    # and finally reload the application
-    restart()
-    return
-
-
 def configure():
     """
     Upload config files, and restart server.
@@ -113,15 +66,15 @@ def configure():
         sudo('rm /etc/nginx/sites-enabled/default')
 
     # upload nginx server blocks (virtualhost)
-    put(env['config_dir'] + '/nginx.conf', '/tmp/nginx.conf')
-    sudo('mv /tmp/nginx.conf /var/www/odac-idp/nginx.conf')
+    put(env.config_dir + '/nginx.conf', '/tmp/nginx.conf')
+    sudo('mv /tmp/nginx.conf %s/nginx.conf' % env.project_dir)
 
     with settings(warn_only=True):
-        sudo('ln -s /var/www/odac-idp/nginx.conf /etc/nginx/conf.d/')
+        sudo('ln -s %s/nginx.conf /etc/nginx/conf.d/' % env.project_dir)
 
     # upload uwsgi config
-    put(env['config_dir'] + '/uwsgi.ini', '/tmp/uwsgi.ini')
-    sudo('mv /tmp/uwsgi.ini /var/www/odac-idp/uwsgi.ini')
+    put(env.config_dir + '/uwsgi.ini', '/tmp/uwsgi.ini')
+    sudo('mv /tmp/uwsgi.ini %s/uwsgi.ini' % env.project_dir)
 
     # make directory for uwsgi's log
     with settings(warn_only=True):
@@ -131,27 +84,67 @@ def configure():
         sudo('mkdir -p /etc/uwsgi/vassals')
 
     # upload upstart configuration for uwsgi 'emperor', which spawns uWSGI processes
-    put(env['config_dir'] + '/uwsgi.conf', '/tmp/uwsgi.conf')
+    put(env.config_dir + '/uwsgi.conf', '/tmp/uwsgi.conf')
     sudo('mv /tmp/uwsgi.conf /etc/init/uwsgi.conf')
 
     with settings(warn_only=True):
         # create symlinks for emperor to find config file
-        sudo('ln -s /var/www/odac-idp/uwsgi.ini /etc/uwsgi/vassals')
+        sudo('ln -s %s/uwsgi.ini /etc/uwsgi/vassals' % env.project_dir)
 
     sudo('chown -R www-data:www-data /var/log/uwsgi')
-    sudo('chown -R www-data:www-data /var/www/odac-idp')
+    sudo('chown -R www-data:www-data ' + env.project_dir)
 
     # upload flask config
     with settings(warn_only=True):
-        sudo('mkdir /var/www/odac-idp/instance')
-    put(env['config_dir'] + '/config.py', '/tmp/config.py')
-    sudo('mv /tmp/config.py /var/www/odac-idp/instance/config.py')
+        sudo('mkdir %s/instance' % env.project_dir)
+    put(env.config_dir + '/config.py', '/tmp/config.py')
+    sudo('mv /tmp/config.py %s/instance/config.py' % env.project_dir)
 
     # upload data files
     put('instance/data_budget.json', '/tmp/data_budget.json')
-    sudo('mv /tmp/data_budget.json /var/www/odac-idp/instance/data_budget.json')
+    sudo('mv /tmp/data_budget.json %s/instance/data_budget.json' % env.project_dir)
     put('instance/data_councillors.json', '/tmp/data_councillors.json')
-    sudo('mv /tmp/data_councillors.json /var/www/odac-idp/instance/data_councillors.json')
+    sudo('mv /tmp/data_councillors.json %s/instance/data_councillors.json' % env.project_dir)
 
+    set_permissions()
     restart()
     return
+
+
+def deploy():
+    """
+    Upload our package to the server.
+    """
+
+    # create application directory if it doesn't exist yet
+    with settings(warn_only=True):
+        if run("test -d " + env.project_dir).failed:
+            # create project folder
+            sudo('mkdir -p ' + env.project_dir)
+
+    # create a tarball of our package
+    local('tar -czf frontend.tar.gz frontend/', capture=False)
+
+    # upload the source tarball to the temporary folder on the server
+    put('frontend.tar.gz', '/tmp/frontend.tar.gz')
+
+    # enter application directory
+    with cd(env.project_dir):
+        # and unzip new files
+        sudo('tar xzf /tmp/frontend.tar.gz')
+
+    # now that all is set up, delete the tarball again
+    sudo('rm /tmp/frontend.tar.gz')
+    local('rm frontend.tar.gz')
+
+    sudo('touch %s/frontend/uwsgi.sock' % env.project_dir)
+
+    # clean out old logfiles
+    with settings(warn_only=True):
+        sudo('rm %s/frontend/debug.log*' % env.project_dir)
+
+    set_permissions()
+    restart()
+    return
+
+
